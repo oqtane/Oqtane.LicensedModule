24bc9003638a9f5377fd55aeee1e92c0c0c55c4e
sbwalker <shaun.walker@siliqon.com>
2024-01-19 13:34:22 -0500

    specify licensing assemblies

--- Package/debug.cmd
+++ Package/debug.cmd
@@ -1,2 +1,5 @@
 XCOPY "..\Client\bin\Debug\net8.0\Oqtane.LicensedModule.Client.Oqtane.dll" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net8.0\" /Y
 XCOPY "..\Client\bin\Debug\net8.0\Oqtane.LicensedModule.Client.Oqtane.pdb" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net8.0\" /Y
+XCOPY "..\Client\bin\Debug\net8.0\Oqtane.Licensing.Client.Oqtane.dll" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net8.0\" /Y
+XCOPY "..\Client\bin\Debug\net8.0\Oqtane.Licensing.Server.Oqtane.dll" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net8.0\" /Y
+XCOPY "..\Client\bin\Debug\net8.0\Oqtane.Licensing.Shared.Oqtane.dll" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net8.0\" /Y
